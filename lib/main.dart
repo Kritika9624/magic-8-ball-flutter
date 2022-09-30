d8c1c5d171774cf5cf0ec4089d81575bc7f5b23d
kritika9624 <kritikasingh35@gmail.com>
2022-09-30 18:40:31 +0530

    created magic ball app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.deepOrange.shade500,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int answer = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  answer = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$answer.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
